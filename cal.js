4f780386f7b03c0991da23eecce008805a7aa8ad
Hussaini Bello <hussainib237@gmail.com>
2025-10-01 13:45:56 +0100

    my first commit

---
+++ cal.js
@@ -0,0 +1,23 @@
+function calculate(operation) {
+    let num1 = parseFloat(document.getElementById('num1').value);
+    let num2 = parseFloat(document.getElementById('num2').value);
+    let result 0;}
+    if (isNaN(num1) || isNaN(num2)) {
+        document.getElementById('result').innerText = 'enter valid numbers.';
+        return;
+    }
+    switch (operation) {
+        case 'add':
+            result = num1 + num2;
+            break;
+        case 'subtract':
+            result = num1 - num2;
+            break;
+        case 'multiply':
+            result = num1 * num2;
+            break;
+        case 'divide':
+        result = num1 / num2;
+            break;
+    }
+    document.getElementById('result').innerText = result;
